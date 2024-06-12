f13d7a1d96b6feda55d66fa1cda329e1a651f59c
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-12 21:44:53 +0800

    adding table for medical records and appointments with column

---
+++ database/migrations/2024_06_12_133813_create_appointments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id');
+            $table->unsignedBigInteger('patient_id');
+            $table->string('doctor_name');
+            $table->string('patient_name');
+            $table->date('date');
+            $table->timestamps();
+        
+            $table->foreign('doctor_id')->references('user_id')->on('doctors')->onDelete('cascade');
+            $table->foreign('patient_id')->references('user_id')->on('patients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
---
+++ database/migrations/2024_06_12_133841_create_medical_records_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_records', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->string('patient_name'); 
+            $table->text('description');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('patient_id')->references('user_id')->on('patients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_records');
+    }
+};
