ca68dbe5b3903008beea6f953b995c690c4683ac
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-12 21:31:54 +0800

    adding fillable and function to the doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
