ccb2e76585f7d2ab6ba318ae2fe5ec392f1c551d
Mark Joshua Dueñas <duenasmarkjoshua95@gmail.com>
2024-06-12 20:09:35 +0800

    adding medical records api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicalRecordController;
+
 
 
 
@@ -21,4 +23,9 @@ Route::get('/patients', [PatientController::class, 'index']);
     Route::put('/patients/{id}', [PatientController::class, 'update']);
     Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
 
+    Route::get('/records', [MedicalRecordController::class, 'index']);
+    Route::post('/records', [MedicalRecordController::class, 'store']);
+    Route::get('/records/{id}', [MedicalRecordController::class, 'show']);
+    Route::put('/records/{id}', [MedicalRecordController::class, 'update']);
+    Route::delete('/records/{id}', [MedicalRecordController::class, 'destroy']);
 
