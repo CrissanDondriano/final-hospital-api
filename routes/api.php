a43e6106215e869bb1879147b1b73ef1245c5869
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-12 21:23:55 +0800

    adding doctor controller

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,14 @@ use App\Http\Controllers\MedicalRecordController;
 
 
 Route::get('/patients', [PatientController::class, 'index']);
-    Route::post('/patients', [PatientController::class, 'store']);
-    Route::get('/patients/{id}', [PatientController::class, 'show']);
-    Route::put('/patients/{id}', [PatientController::class, 'update']);
-    Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
+Route::post('/patients', [PatientController::class, 'store']);
+Route::get('/patients/{id}', [PatientController::class, 'show']);
+Route::put('/patients/{id}', [PatientController::class, 'update']);
+Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
 
-    Route::get('/records', [MedicalRecordController::class, 'index']);
-    Route::post('/records', [MedicalRecordController::class, 'store']);
-    Route::get('/records/{id}', [MedicalRecordController::class, 'show']);
-    Route::put('/records/{id}', [MedicalRecordController::class, 'update']);
-    Route::delete('/records/{id}', [MedicalRecordController::class, 'destroy']);
+Route::get('/records', [MedicalRecordController::class, 'index']);
+Route::post('/records', [MedicalRecordController::class, 'store']);
+Route::get('/records/{id}', [MedicalRecordController::class, 'show']);
+Route::put('/records/{id}', [MedicalRecordController::class, 'update']);
+Route::delete('/records/{id}', [MedicalRecordController::class, 'destroy']);
 
