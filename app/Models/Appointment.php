77af9028b7149ade045af72641c40e0d30e76cf6
znrxrj <erojozenrix70@gmail.com>
2024-06-12 19:59:40 +0800

    Adding Appointment Model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'doctor_name',
+        'patient_name',
+        'date',
+    ];
+    
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
