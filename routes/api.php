4f6d935dffd6f8d901dd9edb1b7f7acd9cd5007c
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-12 21:36:21 +0800

    adding route for api for doctors and appointments

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\MedicalRecordController;
-
-
-
-
-
-
-
-
-
-
-
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\AppointmentController;
 
 
 Route::get('/patients', [PatientController::class, 'index']);
@@ -22,6 +13,18 @@ Route::post('/patients', [PatientController::class, 'store']);
 Route::get('/patients/{id}', [PatientController::class, 'show']);
 Route::put('/patients/{id}', [PatientController::class, 'update']);
 Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
+
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::post('/doctors', [DoctorController::class, 'store']);
+Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+Route::put('/doctors/{id}', [DoctorController::class, 'update']);
+Route::delete('/doctors/{id}', [DoctorController::class, 'destroy']);
+
+Route::get('/appointments', [AppointmentController::class, 'index']);
+Route::post('/appointments', [AppointmentController::class, 'store']);
+Route::get('/appointments/{id}', [AppointmentController::class, 'show']);
+Route::put('/appointments/{id}', [AppointmentController::class, 'update']);
+Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy']);
 
 Route::get('/records', [MedicalRecordController::class, 'index']);
 Route::post('/records', [MedicalRecordController::class, 'store']);
